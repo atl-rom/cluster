8fee2d3f69aea04fbe412aba47199ce70c71b344
atl-rom <77552184+atl-rom@users.noreply.github.com>
2021-02-22 17:14:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_launch_configuration" "example" {
   user_data = data.template_file.user_data.rendered
   
 
-  #using input file as alternative instead of inline user data
+  #using input file as alternative instead of inline userd data
   
   
   # user_data =<<-EOF
